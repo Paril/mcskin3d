395c6416cb72849cdca9b0d939cced09d8f4d2bd
webmaster@xylemstudios.com <webmaster@xylemstudios.com@aea06634-d2fb-6e8f-d1be-234d417fb63c>
2011-11-09 22:16:39 +0000

    Added threshold to FloodFillTool.cs

--- Tools/FloodFillTool.cs
+++ Tools/FloodFillTool.cs
@@ -8,8 +8,14 @@ using System.Windows.Forms;
 
 namespace MCSkin3D
 {
+    //Added threshold [Xylem] 09/11/2011
 	public class FloodFillTool : ITool
 	{
+        public float Threshold //[0-1]
+        {
+            get { return 0.2f; }
+        }
+
 		PixelsChangedUndoable _undo;
 		private bool[] hitPixels;
 
@@ -21,6 +27,28 @@ namespace MCSkin3D
 		public void MouseMove(Skin skin, MouseEventArgs e)
 		{
 		}
+
+        private byte fixValue(int v)
+        {
+            //fix f-ed up (out of bounds) values in isCloseColor function
+            if (v < 0)
+                return 0;
+
+            if (v > 255)
+                return 255;
+
+            return (byte)v;
+        }
+        private bool isCloseColor(Color c, Color c2, float threshold)
+        {
+            //c is origin color, c2 is color to compare to, t is the threshold converted from percent to byte
+            byte t = (byte)(threshold * 255 /2);
+            bool isR, isG, isB;
+            isR = ((fixValue(c.R - t) <= c2.R) && (fixValue(c.R + t) >= c2.R));
+            isG = ((fixValue(c.G - t) <= c2.G) && (fixValue(c.G + t) >= c2.G));
+            isB = ((fixValue(c.B - t) <= c2.B) && (fixValue(c.B + t) >= c2.B));
+            return isR && isG && isB;
+        }
 
 		private void recursiveFill(int x, int y, Color oldColor, Color newColor, int[] pixels, bool[] hitPixels, Skin skin)
 		{
@@ -34,7 +62,7 @@ namespace MCSkin3D
 			var c = pixels[i];
 			var real = Color.FromArgb((c >> 24) & 0xFF, (c >> 0) & 0xFF, (c >> 8) & 0xFF, (c >> 16) & 0xFF);
 
-			if (real != oldColor)
+            if (!isCloseColor(oldColor, real, Threshold))
 				return;
 
 			if (!_undo.Points.ContainsKey(new Point(x, y)))
@@ -43,13 +71,13 @@ namespace MCSkin3D
 			pixels[i] = newColor.R | (newColor.G << 8) | (newColor.B << 16) | (newColor.A << 24);
 			hitPixels[i] = true;
 
-			recursiveFill(x, y - 1, oldColor, newColor, pixels, hitPixels, skin);
+            recursiveFill(x, y - 1, oldColor, newColor, pixels, hitPixels, skin);
 			//recursiveFill(x + 1, y - 1, oldColor, newColor, pixels, hitPixels, skin);
-			recursiveFill(x + 1, y, oldColor, newColor, pixels, hitPixels, skin);
+            recursiveFill(x + 1, y, oldColor, newColor, pixels, hitPixels, skin);
 			//recursiveFill(x + 1, y + 1, oldColor, newColor, pixels, hitPixels, skin);
-			recursiveFill(x, y + 1, oldColor, newColor, pixels, hitPixels, skin);
+            recursiveFill(x, y + 1, oldColor, newColor, pixels, hitPixels, skin);
 			//recursiveFill(x - 1, y + 1, oldColor, newColor, pixels, hitPixels, skin);
-			recursiveFill(x - 1, y, oldColor, newColor, pixels, hitPixels, skin);
+            recursiveFill(x - 1, y, oldColor, newColor, pixels, hitPixels, skin);
 			//recursiveFill(x - 1, y - 1, oldColor, newColor, pixels, hitPixels, skin);
 		}
 
@@ -61,7 +89,7 @@ namespace MCSkin3D
 			var oldColor = Color.FromArgb((c >> 24) & 0xFF, (c >> 0) & 0xFF, (c >> 8) & 0xFF, (c >> 16) & 0xFF);
 			var newColor = Program.MainForm.SelectedColor;
 
-			recursiveFill(x, y, oldColor, newColor, pixels, hitPixels, skin);
+            recursiveFill(x, y, oldColor, newColor, pixels, hitPixels, skin);
 			return true;
 		}
 
